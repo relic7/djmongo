17ff5830200bf37af6767f2f524d4a81d4a57c69
johnb <john.bragato@gmail.com>
2014-12-10 16:06:26 -0500

    using xenith django-base-template structure

---
+++ djmongo/settings/test.py
@@ -0,0 +1,21 @@
+"""
+This is an example settings/test.py file.
+Use this settings file when running tests.
+These settings overrides what's in settings/base.py
+"""
+
+from .base import *
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
+
+SECRET_KEY = 'rf$_4jl5)qr=o8_fa-cr2v#ozy@6#67a*w26^ut2)fv7ef0=h#'
